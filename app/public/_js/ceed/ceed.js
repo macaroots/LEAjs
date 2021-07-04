a156822290957039c1f2e84f6cbccecf577cc6cc
Renato <macaroots@gmail.com>
2021-07-04 00:58:52 -0300

    Add help

--- app/public/_js/ceed/ceed.js
+++ app/public/_js/ceed/ceed.js
@@ -392,10 +392,51 @@ function NaiveLive() {
 }
 
 function Help() {
-	this.act = function (args, callback) {
-//console.log('Naive.live', this.agent.toString());
-		callback(this.agent);
-	}
+	this.act = async function (args, resolve, reject) {
+		const agent = this.agent;
+		const agents = {
+		    'known': {},
+		    'learnable': {}
+		};
+		{
+    		const brain = agent.mind.getBrain();
+    		const links = brain.reason();
+  		
+    		for (let i in links) {
+    		    let link = links[i];
+    		    let actions = agents['known'][link.a.info];
+    		    if (!actions) {
+    		        actions = [];
+    		        agents['known'][link.a.info] = actions;
+    		    }
+    		    if (!actions.includes(link.r.info)) {
+    		        actions.push(link.r.info);
+    		    }
+    		    
+    		}
+		}
+		
+		const brains = await agent.see('getLibraries');
+		for (let brain of brains) {
+    		const links = brain.reason();
+    		
+    		for (let i in links) {
+    		    let link = links[i];
+    		    let actions = agents['learnable'][link.a.info];
+    		    if (!actions) {
+    		        actions = [];
+    		        agents['learnable'][link.a.info] = actions;
+    		    }
+    		    if (!actions.includes(link.r.info)) {
+    		        actions.push(link.r.info);
+    		    }
+    		    
+    		}
+		}
+		
+		console.log('help', agents);
+		resolve(agents);
+	};
 }
 
 function Live() {
