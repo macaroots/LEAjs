fe2ad323fd238affa074b4911caf94ae7feddbf8
Renato <macaroots@gmail.com>
2021-08-25 11:59:40 -0300

    Versao com MySQL

--- app/app.js
+++ app/app.js
@@ -7,35 +7,35 @@
  */
 import {Ceed} from './public/_js/ceed/ceed.js';
 import {FileBrain} from './public/_js/ceed/file_brain.js';
-//import {} from './lib/lea.js';
 /**/
+import {} from './lib/lea.js';
+/*/
 let library = new FileBrain('./live');
 const ceed = await Ceed();
 await ceed.see('addLibrary', library);
 /**/
 
 const lea = await Ceed('LEA');
-/**/
+try {
 /*/
-lea.see('initHttpBrain', {
-	host: process.env.BRAIN_HOST,
-	protocol: process.env.BRAIN_PROTOCOL
-}).then(() => {
-/* /
-lea.see('initBrain', {
-	host: process.env.MYSQL_HOST || 'localhost',
-	database: process.env.MYSQL_DATABASE || 'leajs',
-	user: process.env.MYSQL_USER || 'root',
-	password: process.env.MYSQL_PASSWORD || 'admin',
-    // debug: true
-}).then(async () => {
+    await lea.see('initHttpBrain', {
+        host: process.env.BRAIN_HOST || 'http://localhost/brain',
+        protocol: process.env.BRAIN_PROTOCOL || 'http'
+    });
+/*/
+    await lea.see('initBrain', {
+        host: process.env.MYSQL_HOST || 'localhost',
+        database: process.env.MYSQL_DATABASE || 'leajs',
+        user: process.env.MYSQL_USER || 'root',
+        password: process.env.MYSQL_PASSWORD || 'admin',
+        // debug: true
+    });
 /**/
 
-    lea.see('listen', {
+    await lea.see('listen', {
         hostname: process.env.HOSTNAME || '127.0.0.1', 
         port: process.env.PORT || 3000
     });
-/** /
-}).catch(e => {
+} catch(e) {
 	console.error('ERROR LEA APP', e);
-});/**/
+}
