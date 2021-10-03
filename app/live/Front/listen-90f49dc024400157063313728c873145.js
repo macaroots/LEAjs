f79ea9e27f728e64080c083ab7c83ba56d422583
Renato <macaroots@gmail.com>
2021-10-03 15:46:59 -0300

    Fix HTTPS

--- app/live/Front/listen-90f49dc024400157063313728c873145.js
+++ app/live/Front/listen-90f49dc024400157063313728c873145.js
@@ -1,24 +1,24 @@
 new (function Listen() {
-	this.act = async function(options, callback) {
-		let defaults = {
-			hostname: '127.0.0.1',
-			port: 3000,
-			dir: 'public'
-		};
-		options = Object.assign(defaults, options);
+    this.act = async function(options, callback) {
+        let defaults = {
+            hostname: '127.0.0.1',
+            port: 3000,
+            dir: 'public'
+        };
+        options = Object.assign(defaults, options);
 
-		let agent = this.agent;
-		
-		let http = await import('http');
-		let express = (await import('express')).default;
-		const app = express();
-		const server = http.createServer(app);
-        // TODO não deixa ligar vários servidores
-		agent.app = app;
-		agent.server = server;
+        let agent = this.agent;
         
-		// Setting up the public directory
-		app.use(express.static(options.dir));
+        let http = await import('http');
+        let express = (await import('express')).default;
+        const app = express();
+        const server = http.createServer(app);
+        // TODO não deixa ligar vários servidores
+        agent.app = app;
+        agent.server = server;
+        
+        // Setting up the public directory
+        app.use(express.static(options.dir));
         /**/
         // Sessão
         const session = (await import('express-session')).default;
@@ -30,43 +30,43 @@ new (function Listen() {
         });
         app.use(sessionParser);
         /**/
-		
-		// Setting up POST parser
-		let bodyParser = (await import('body-parser')).default;
-		app.use(bodyParser.json());
-		app.use(bodyParser.urlencoded({ extended: true }));
-		
-		let io = (await import('socket.io')).default;
-		let sio = io(server);
-		agent.io = sio;
-		sio.on('connection', (socket) => {
-			agent.see('onSocketConnection', socket);
-		});
+        
+        // Setting up POST parser
+        let bodyParser = (await import('body-parser')).default;
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: true }));
+        
+        let io = (await import('socket.io')).default;
+        let sio = io(server);
+        agent.io = sio;
+        sio.on('connection', (socket) => {
+            agent.see('onSocketConnection', socket);
+        });
         sio.use(function(socket, next) {
             sessionParser(socket.request, socket.request.res || {}, next);
         });
 
-		// Rotas
+        // Rotas
         // TODO colocar numa ação
-		/**/
+        /**/
         var router = express.Router();
-		router.all([
-		    '/',
-		    '/:agent',
-		    '/:agent/:action',
-		    '/:agent/:action/*'
-		], (req, res) => {
-			agent.see('http', [req, res]);
-		});
-		app.use('/', router);
-		/**/
+        router.all([
+            '/',
+            '/:agent',
+            '/:agent/:action',
+            '/:agent/:action/*'
+        ], (req, res) => {
+            agent.see('http', [req, res]);
+        });
+        app.use('/', router);
+        /**/
 
-		server.listen(options.port, options.hostname, () => {
-		  console.log(`Server running at http://${options.hostname}:${options.port}/`);
-		});
+        server.listen(options.port, options.hostname, () => {
+        console.log(`Server running at http://${options.hostname}:${options.port}/`);
+        });
         
         agent.see('notify', 'serverInitialized');
-		
-		callback();
-	}
+        
+        callback();
+    }
 })();
