4a41763854ca5a58dc0ca93cf35c32c01bf39f74
Renato <macaroots@gmail.com>
2021-10-12 00:33:29 -0300

    Separacao por / Usuario/Agente/acao

---
+++ app/live/Front/http-a34e9186328c71dd2f978f660dafd99b.js
@@ -0,0 +1,23 @@
+new (function Http() {
+	this.act = async function (args, callback) {
+		let [req, res] = args;
+console.log(this.agent + 'http', req.params);		
+	    let agentName = req.params.agent || 'index';
+	    let actionName = req.params.action || 'index';
+		
+		res.locals.agentName = agentName;
+		res.locals.actionName = actionName;
+		
+		try {
+            const agent = await Ceed(agentName + '/Controller Controller');
+			//await agent.see('live');
+			await agent.see('preDispatch', args);
+			await agent.see(actionName, args);
+			await agent.see('postDispatch', args);
+		} catch(e) {
+			console.log('ERRO HTTP', e);
+		};
+		
+		callback(true); // return statement
+	};
+})();
