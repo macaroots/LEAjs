f5473c804b84ba7b67115698099281084845d532
Renato <macaroots@gmail.com>
2021-11-25 19:40:28 -0300

    Fix username nao pode ter ponto, tem _

---
+++ app/live/Auth/Controller/googleLogin-3210ac3c49ce0aba9d6eb7689d65cafa.js
@@ -0,0 +1,35 @@
+new (function googleLogin() {
+	this.act = async function (args, resolve, reject) {
+		let req = args[0];
+		let res = args[1];
+		let agent = this.agent;
+		const token = req.body.id;
+console.log('TOKEN', token);
+		
+		const CLIENT_ID = await agent.see('CLIENT_ID');
+		
+		const {OAuth2Client} = (await import('google-auth-library')).default;
+        const client = new OAuth2Client(CLIENT_ID);
+        async function verify() {
+            const ticket = await client.verifyIdToken({
+              idToken: token,
+              audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+              // Or, if multiple clients access the backend:
+              //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+            });
+            const payload = ticket.getPayload();
+            const user = payload['email'].split('@')[0].replace(new RegExp('\\.', 'g'), '_');
+          
+console.log('GOOGLE_LOGIN', user);
+		    req.session.user = user;
+		    //res.json({ok: true});
+		    res.redirect('/' + user);
+        }
+        verify().catch(e => {
+		    req.session.destroy();
+            res.status(403).json({ok: false, error: 'Invalid ID token.', e: e});
+        });
+		
+		resolve();
+	};
+})();
---
+++ app/live/Auth/Controller/login-6d1f9df496f9b7e9ccddd98fe660d24b.js
@@ -0,0 +1,24 @@
+new (function login() {
+	this.act = function (args, resolve, reject) {
+		let [req, res] = args;
+		let username = req.body.username.replace(new RegExp('\\.', 'g'), '_');
+		let password = req.body.password;
+		
+		let ok = false;
+		if (password == username + '!@#') {
+		    req.session.user = username;
+		    ok = true;
+		    
+		    res.redirect('/' + username);
+		}
+		else {
+		    req.session.destroy();
+		    
+            res.status(403).json({ok: false, error: 'Invalid crendentials.'});
+		}
+	    //res.send({ok: ok});
+	    
+		
+		resolve();
+	};
+})();
