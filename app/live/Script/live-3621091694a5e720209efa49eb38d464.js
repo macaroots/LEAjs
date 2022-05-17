6dfed98d6c07cd8911c474c9c4571086e72d1107
Renato <macaroots@gmail.com>
2022-05-16 23:07:14 -0300

    Fix ace loading

---
+++ app/live/Script/live-3621091694a5e720209efa49eb38d464.js
@@ -0,0 +1,47 @@
+new (function live () {
+	this.act = async function (target, resolve, reject) {
+console.log('SCRIPT LIVE', this.agent.toString());
+		let agent = this.agent;
+		await agent.see('addName', 'Script Element');
+		let config = function () { 
+			ace.config.set('modePath', 'https://pagecdn.io/lib/ace/1.4.12/');
+			ace.config.set('workerPath', 'https://pagecdn.io/lib/ace/1.4.12/');
+			ace.config.set('themePath', 'https://pagecdn.io/lib/ace/1.4.12/');
+			
+			resolve(true);
+		};
+	    let loadAce = new Promise((resolve, reject) => {
+			$.ajax({
+				type: "GET",
+				url: 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.5.0/ace.min.js',
+				dataType: "script",
+				cache: true,
+				success: config,
+                error: function(XMLHttpRequest, textStatus, errorThrown) { 
+                    console.log("Couldn't load ace. Trying another source..."); 
+                    $.ajax({
+        				type: "GET",
+        				url: 'https://pagecdn.io/lib/ace/1.4.12/ace.min.js',
+        				dataType: "script",
+        				cache: true,
+        				success: config
+                    });
+                }     
+			});
+	    });
+	    await Promise.all([
+	        loadAce,
+			agent.see('study', 'html'),
+			agent.see('study', 'reloadHtml'),
+			agent.see('study', 'getHtml'),
+			agent.see('study', 'appendTo'),
+			agent.see('study', 'trigger'),
+			agent.see('study', 'onLoad'),
+	        agent.see('study', 'askFor'),
+			agent.see('study', 'askBind')
+		]);
+		
+		agent.see('listenWindow');
+		resolve(true);
+	};
+})();
