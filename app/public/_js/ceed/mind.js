d9dcd4e02dff6482d78b16ebdd55b47ca68f9f42
Renato <macaroots@gmail.com>
2023-01-26 11:08:01 -0300

    Fix error message

--- app/public/_js/ceed/mind.js
+++ app/public/_js/ceed/mind.js
@@ -359,7 +359,7 @@ function Error(mind) {
 			let e = args[1];
 			let action = args[0][0];
 			let target = args[0][1];
-			console.trace(mind.toString(), e);
+			console.error(mind.toString(), e.stack);
 		} catch (e) {
 			console.error('CRITICAL ERROR', e);
             reject(e)
