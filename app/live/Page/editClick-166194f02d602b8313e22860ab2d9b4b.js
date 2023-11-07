2dd9faf3001fa70072171cc22538d84493a3c256
Renato <macaroots@gmail.com>
2023-11-07 07:17:40 -0300

    Fix Page.editClick

--- app/live/Page/editClick-166194f02d602b8313e22860ab2d9b4b.js
+++ app/live/Page/editClick-166194f02d602b8313e22860ab2d9b4b.js
@@ -1,14 +1,21 @@
 new (function editClick() {
+    let edited = false;
 	this.act = async function (event, resolve, reject) {
 	    let agent = this.agent;
 	    if (!event.ctrlKey) {
 	        let ce = await Ceed('CE');
 	        if (!document.querySelector('.CE')) {
-	            ce.see('appendTo', 'body');
-	            agent.see('edit');
+	            await ce.see('appendTo', 'body');
 	        }
 	        else {
-	            ce.see('toggle');
+	            await ce.see('toggle');
+	        }
+	        if (!edited) {
+                let body = await ce.see('getBody');
+	            if (body[0].style.display != 'none') {
+    	            agent.see('edit');
+    	            edited = true;
+	            }
 	        }
 	    }
 	    else {
